5bfdb60b4a56a206d0429f4eec6074ff60bb051f
Glaxy <90437693+QingyeSC@users.noreply.github.com>
2025-03-22 03:44:54 +0800

    Update token.go
    修改计数器

--- core/gin/response/token.go
+++ core/gin/response/token.go
@@ -1,29 +1,26 @@
 package response
 
 import (
-	"chatgpt-adapter/core/logger"
-	encoder "github.com/samber/go-gpt-3-encoder"
+    "chatgpt-adapter/core/logger"
+    "github.com/pkoukk/tiktoken-go"
 )
 
 func CalcTokens(content string) int {
-	resolver, err := encoder.NewEncoder()
-	if err != nil {
-		logger.Error(err)
-		return 0
-	}
-	result, err := resolver.Encode(content)
-	if err != nil {
-		logger.Error(err)
-		return 0
-	}
-	return len(result)
+    encoder, err := tiktoken.GetEncoding("cl100k_base") // GPT-4 使用的编码
+    if err != nil {
+        logger.Error(err)
+        return 0
+    }
+    
+    tokens := encoder.Encode(content, nil, nil)
+    return len(tokens)
 }
 
 func CalcUsageTokens(content string, previousTokens int) map[string]interface{} {
-	tokens := CalcTokens(content)
-	return map[string]interface{}{
-		"completion_tokens": tokens,
-		"prompt_tokens":     previousTokens,
-		"total_tokens":      previousTokens + tokens,
-	}
+    tokens := CalcTokens(content)
+    return map[string]interface{}{
+        "prompt_tokens":     previousTokens,
+        "completion_tokens": tokens,
+        "total_tokens":      previousTokens + tokens,
+    }
 }
